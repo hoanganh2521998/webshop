eb3a3b85dc114f5cf5662c9f3d85cb257fdafba5
Anh. Nguyen Hoang - CMC Global DU3.11 <nhanh21@cmcglobal.vn>
2022-10-06 16:05:44 +0700

    Upload

---
+++ src/app/pages/cart/cart.component.ts
@@ -0,0 +1,92 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { loadStripe } from '@stripe/stripe-js';
+import { Cart, CartItem } from 'src/app/models/cart.model';
+import { CartService } from 'src/app/services/cart.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styles: [
+  ]
+})
+export class CartComponent implements OnInit {
+  cart: Cart = { items: [{
+    product: 'https://via.placeholder.com/150',
+    name: 'snickers',
+    price: 150,
+    quantity: 1,
+    id: 1,
+  },
+  {
+    product: 'https://via.placeholder.com/150',
+    name: 'snickers',
+    price: 150,
+    quantity: 2,
+    id: 2,
+  }
+]};
+  dataSource: Array<CartItem> = [];
+  displayedColumns: Array<string> = [
+    'product',
+    'name',
+    'price',
+    'quantity',
+    'total',
+    'action'
+  ];
+
+  constructor(private cartService: CartService, private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.cartService.cart.subscribe((_cart: Cart) => {
+      this.cart = _cart;
+      this.dataSource = this.cart.items;
+    });
+  }
+
+  getTotal(items: Array<CartItem>): number {
+    // return items.map((item) => item.price * item.quantity).reduce((prev, current) => prev + current,0);
+    return this.cartService.getTotal(items);
+  }
+
+  onClearCart() {
+    this.cartService.clearCart();
+  }
+
+  onRemoveFromCart(item: CartItem): void {
+    this.cartService.removeFromCart(item);
+  }
+
+  onAddQuantity(item: CartItem): void {
+    this.cartService.addToCart(item);
+  }
+
+  onRemoveQuantity(item: CartItem): void {
+    this.cartService.removeQuantity(item);
+  }
+  
+  // onCheckout(): void {
+  //   this.http.post('http://localhost:4242/checkout', {
+  //     items: this.cart.items
+  //   }).subscribe(async(res: any) => {
+  //     let stripe = await loadStripe('pk_test_51LpolyIBD0RK1i3Et0T9UN774gVclhxtr88squp8FXv14gVK3PsKZTDmqZ6LriEDCicJWOWO38DdFD3u1mFi3qfo00lV2F2Xg3');
+  //     stripe?.redirectToCheckout({
+  //       sessionId: res.id,
+  //     });
+  //   });
+  // }
+
+  onCheckout(): void {
+    this.http
+      .post('http://localhost:4242/checkout', {
+        items: this.cart.items,
+      })
+      .subscribe(async (res: any) => {
+        let stripe = await loadStripe('pk_test_51LpolyIBD0RK1i3Et0T9UN774gVclhxtr88squp8FXv14gVK3PsKZTDmqZ6LriEDCicJWOWO38DdFD3u1mFi3qfo00lV2F2Xg3');
+        stripe?.redirectToCheckout({
+          sessionId: res.id,
+        });
+      });
+  }
+}
